efd0ef0ada79968c54c35b4a5fae72a59cd553f6
qnnguyen86 <nhqb612@gmail.com>
2022-01-05 14:17:28 +0700

    Create Connection.php

---
+++ application/Connection.php
@@ -0,0 +1,13 @@
+<?php 
+
+    class Connection{
+        public static function getInstance(){
+
+            $conn = new PDO("mysql:host=localhost; dbname=train_database","root","");
+
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+            $conn->exec("set names utf8");
+            return $conn;
+        }
+    }
+  ?>
