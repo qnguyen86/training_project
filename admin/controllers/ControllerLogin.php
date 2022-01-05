95c75c84699a2342635eb073aee6403ae13a61cc
qnnguyen86 <nhqb612@gmail.com>
2022-01-05 14:16:03 +0700

    Create ControllerLogin.php

---
+++ admin/controllers/ControllerLogin.php
@@ -0,0 +1,28 @@
+<?php
+    class ControllerLogin extends Controller{
+        public function index(){
+            $this->loadView("ViewLogin.php");
+        }
+        public function login(){
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            $conn = Connection::getInstance();
+
+            $query = $conn->prepare("select email from admin where email= :var_email and password = :var_password");
+            $query->execute(array("var_email"=>$email,"var_password"=>$password));
+            if($query->rowCount() > 0){
+
+            $_SESSION["email"] = $email;
+            header("location:index.php");
+            }
+            else
+            header("location:index.php?controller=login");
+        }
+        //logout
+        public function logout(){
+            unset($_SESSION["email"]);
+            header("location:index.php");
+        }
+    }
+?>
