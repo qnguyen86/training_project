053191cee62e4e318f78cfef9ba7efba0a39b155
qnnguyen86 <nhqb612@gmail.com>
2022-01-05 14:17:20 +0700

    Create ViewHome.php

---
+++ admin/views/ViewHome.php
@@ -0,0 +1,10 @@
+<?php
+    $this->fileLayout = "Layout.php";
+?>
+<h2>HomeView</h2>
+<section class="content">
+    <!-- Small boxes (Stat box) -->
+    <!--  -->
+    
+    <!-- /.row -->
+  </section>
