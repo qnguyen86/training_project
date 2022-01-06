91c3ff4f2e9dd58b9a7780ca2fb876b55f759b84
qnnguyen86 <nhqb612@gmail.com>
2022-01-06 17:01:07 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once('connection.php');
+
+if (isset($_GET['controller']))
+{
+    $controller = $_GET['controller'];
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+    } else {
+        $action = 'index';
+    }
+} else {
+    $controller = 'admin';
+    $action = 'login';
+}
+require_once('routes.php');
