6cb2f2ede7757ae5184fb1636d4af1d3ca04a47f
qnnguyen86 <nhqb612@gmail.com>
2022-01-20 17:04:07 +0700

    Create BaseModel.php

---
+++ models/BaseModel.php
@@ -0,0 +1,15 @@
+<?php
+require_once "DBInterface.php";
+abstract class BaseModel implements DBInterface
+{
+   // protected $tableName = '';
+    public function insert($data)
+    {
+
+
+    }
+
+    public function update() {}
+
+    public function delete() {}
+}
