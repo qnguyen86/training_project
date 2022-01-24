3c374d66f47c127cdfa7997473cabb3e605ad31c
qnnguyen86 <nhqb612@gmail.com>
2022-01-24 16:56:35 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 $controllers = array(
-    'admin' =>['index','error','login','create']
+    'admin' =>['index','error','login','create'],
+    'user' =>['index','error','create']
 ); // Các controllers trong hệ thống và các action có thể gọi ra từ controller đó.
 
 // Nếu các tham số nhận được từ URL không hợp lệ (không thuộc list controller và action có thể gọi
