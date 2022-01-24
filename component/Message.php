210b92298d8f809600891871432d70eaa672ceb4
qnnguyen86 <nhqb612@gmail.com>
2022-01-24 16:55:43 +0700

    Create Message.php

---
+++ component/Message.php
@@ -0,0 +1,11 @@
+<?php
+define('ERROR_PASSWORD_ONE', 'Please enter your password');
+define('ERROR_PASSWORD_TWO', 'Password must be at least six characters');
+define('ERROR_EMAIl', 'Please enter email');
+define('ERROR_NAME','Please enter name');
+define('ERROR_AVATAR','Please enter avatar');
+
+define('INSERT_PASS', 'ADD ITEM SUCCESS');
+define('INSERT_FAIL', 'An error occurred, please try again');
+?>
+
