237e13c9a34d716d91ffe0cbb0b2235b82cdef77
qnnguyen86 <nhqb612@gmail.com>
2022-01-25 17:07:38 +0700

    Update UserModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -11,7 +11,26 @@ class UserModel extends BaseModel
         $this->db = DB::getInstance();
         $this->tableName = "users";
     }
+    public function modelRead($recordPerPage)
+    {
+        $page = isset($_GET["page"]) && $_GET["page"] > 0 ? $_GET["page"] - 1 : 0;
+        $from = $page * $recordPerPage;
+        $conn = DB::getInstance();
+        $query = $conn->query("select * from users order by id desc limit $from, $recordPerPage");
+        return $query->fetchAll();
+    }
 
+    public function modelTotalRecord()
+    {
+        $conn = DB::getInstance();
+        $query = $conn->query("select id from users");
+        return $query->rowCount();
+    }
+    public function modelGetID($id)
+    {
+        return $this->db->query("SELECT * FROM `{$this->tableName}` WHERE `id` = '{$id}' ")->fetch();
+
+    }
 
 
 
