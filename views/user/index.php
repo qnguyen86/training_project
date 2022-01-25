75a440daf1c926cdffcc035523ae9ca0ccc9efad
qnnguyen86 <nhqb612@gmail.com>
2022-01-25 17:07:55 +0700

    Update index.php

--- views/user/index.php
+++ views/user/index.php
@@ -10,8 +10,56 @@
 </head>
 <body>
 <?php include "views/layout/header.php"?>
+<div class="col-md-12">
+    <div class="panel-heading"><b>List of users</b></div>
+</div>
+<div class="panel-body">
+    <table class="table table-bordered table-hover">
+        <tr>
+            <th style="width: 100px;">Id</th>
+            <th style="width: 100px;">Avatar</th>
+            <th>Name</th>
+            <th style="width: 100px;">Email</th>
+            <th style="width: 50px;">Status</th>
+            <th style="width:100px;"></th>
+        </tr>
+        <?php
+        foreach($data as $rows):
+            ?>
+            <tr>
+                <td><?php echo $rows['id']; ?></td>
+                <td>
+                    <?php if($rows['avatar']!="" && file_exists("assets/upload/user/".$rows['avatar'])): ?>
+                        <img src="assets/upload/user/<?php echo $rows['avatar']; ?>" style="width: 100px;" alt="" srcset="">
+                    <?php endif; ?>
+                </td>
+                <td><?php echo $rows['name']; ?></td>
+                <td><?php echo $rows['email']; ?></td>
+                <td style="text-align: center;">
+                    <?php if(isset($rows['status'])&&$rows['status'] == 1): ?>
+                        <span >Active</span>
+                    <?php else: ?>
+                        <span>Banned</span>
 
+                    <?php endif; ?>
+                </td>
+                <td style="text-align:center; width: 200px;">
+                    <a href="index.php?controller=user&action=update&id=<?php echo $rows['id']; ?>" class="btn btn-success">Edit</a>&nbsp;
+                    <a href="index.php?controller=user&action=delete&id=<?php echo $rows['id']; ?>" onclick="return window.confirm('Are you sure?');" class="btn btn-warning">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-<div>noidung</div>
+</div>
+<style type="text/css">
+    .pagination{padding:0px; margin:0px;}
+</style>
+<ul class="pagination">
+    <li class="page-item"><a href="#" class="page-link">Page</a></li>
+    <?php for($i = 1; $i <= $numPage; $i++): ?>
+        <li class="page-item"><a href="index.php?controller=user&action=index&page=<?php echo $i; ?>" class="page-link"><?php echo $i; ?></a></li>
+    <?php endfor; ?>
+</ul>
 </body>
 </html>
