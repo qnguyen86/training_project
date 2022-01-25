8d36c42d73b255ea5a633f34a77293d5443fa85d
qnnguyen86 <nhqb612@gmail.com>
2022-01-25 17:07:41 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,19 +1,15 @@
 <?php
 $controllers = array(
-    'admin' =>['index','error','login','create'],
-    'user' =>['index','error','create']
-); // Các controllers trong hệ thống và các action có thể gọi ra từ controller đó.
+    'admin' => ['index', 'error', 'login', 'search', 'create', 'update', 'delete'],
+    'user' => ['index', 'error', 'create', 'update', 'delete']
+);
 
-// Nếu các tham số nhận được từ URL không hợp lệ (không thuộc list controller và action có thể gọi
-// thì trang báo lỗi sẽ được gọi ra.
 if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
     $controller = 'admin';
     $action = 'login';
 }
 
-// Nhúng file định nghĩa controller vào để có thể dùng được class định nghĩa trong file đó
-include_once('controllers/' .ucfirst($controller) . 'Controller.php');
-// Tạo ra tên controller class từ các giá trị lấy được từ URL sau đó gọi ra để hiển thị trả về cho người dùng.
+include_once('controllers/' . ucfirst($controller) . 'Controller.php');
 $klass = str_replace('_', '', ucwords($controller, '_')) . 'Controller';
 $controller = new $klass;
 $controller->$action();
