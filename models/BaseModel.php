670f2b8d529228dd3b6b5e0c51271df2bf09dc2b
qnnguyen86 <nhqb612@gmail.com>
2022-01-25 17:07:35 +0700

    Update BaseModel.php

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,6 +1,7 @@
 <?php
 require_once "DBInterface.php";
 date_default_timezone_set('Asia/Ho_Chi_Minh');
+
 abstract class BaseModel implements DBInterface
 {
     public function __construct()
@@ -8,9 +9,8 @@ abstract class BaseModel implements DBInterface
         $this->db = DB::getInstance();
     }
 
-    public function insert($data=[])
+    public function insert($data = [])
     {
-
         $ins = ['ins_id' => 0, 'ins_datetime' => date("y-m-d H:i:s "),];
         $data = array_merge($data, $ins);
 
@@ -22,12 +22,32 @@ abstract class BaseModel implements DBInterface
         }
         $fields = substr($fields, 0, -2);
         $values = substr($values, 0, -2);
-        echo $sql = "INSERT INTO {$this->tableName} ($fields) VALUES ($values)";
+        $sql = "INSERT INTO {$this->tableName} ($fields) VALUES ($values)";
         $arr = $this->db->query($sql);
         return $arr ? true : false;
     }
 
-    public function update() {}
+    public function update($data = [], $condition = "")
+    {
+        $upd = ['upd_id' => 0, 'upd_datetime' => date("Y-m-d H:i:s "),];
+        $data = array_merge($data, $upd);
+        $sql = "";
+        foreach ($data as $field => $value) {
+            $sql .= "{$field} = '{$value}', ";
+        }
+        $sql = substr($sql, 0, -2);
+        $sql = "UPDATE {$this->tableName} SET $sql WHERE $condition";
+        //UPDATE admin SET name = "NguyenVanB" WHERE id= 15;
+        $arr = $this->db->query($sql);
+        return $arr ? true : false;
+    }
 
-    public function delete() {}
+    public function delete($condition = "")
+    {
+        
+        $sql = "DELETE FROM {$this->tableName} WHERE $condition";
+        $query = $this->db->query($sql);
+        return $query ? true : false;
+    }
+
 }
