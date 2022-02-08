929286ed5880601a1b977d801f4cd036d8fbb7f3
qnnguyen86 <nhqb612@gmail.com>
2022-02-08 17:08:51 +0700

    Create Config.php

---
+++ component/Config.php
@@ -0,0 +1,10 @@
+<?php
+define('ACTIVE', 0);
+define('RECORD_PER_PAGE', 3);
+define('MIN_LENGTH_EMAIL', 10);
+define('MAX_LENGTH_EMAIL', 128);
+define('MIN_LENGTH_NAME', 10);
+define('MAX_LENGTH_NAME', 128);
+define('MIN_LENGTH_PASSWORD', 6);
+define('MAX_LENGTH_PASSWORD', 64);
+?>
