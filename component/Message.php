697a247db9cd183b17cd7ade2343693c3181b525
qnnguyen86 <nhqb612@gmail.com>
2022-02-08 17:08:56 +0700

    Update Message.php

--- component/Message.php
+++ component/Message.php
@@ -2,13 +2,16 @@
 define('ERROR_PASSWORD_ONE', 'Please enter your password');
 define('CONFIRM_PASSWORD', 'password incorrect');
 define('ERROR_PASSWORD_TWO', 'Password must be at least six characters');
+define('ERROR_LENGTH_PASSWORD','length error');
 
 define('ERROR_EMAIl', 'Please enter email');
 define('ERROR_NOT_EMAIl', 'not found email');
 define('ERROR_INVALID_EMAIl', 'Invalid email');
+define('ERROR_LENGTH_EMAIl','length error');
 
 define('ERROR_NAME','Please enter name');
 define('ERROR_INVALID_NAME', 'Invalid name');
+define('ERROR_LENGTH_NAME','length error');
 define('ERROR_AVATAR','Please enter avatar');
 
 define('INSERT_PASS', 'ADD ITEM SUCCESS');
